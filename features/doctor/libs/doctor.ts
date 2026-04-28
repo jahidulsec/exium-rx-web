141630a047cb3f0ffae1f6e2754e5a5f4db66e6b
Jahidul Islam <jahidul.app@gmail.com>
2026-04-28 16:18:55 +0600

    chore: add rx date filter for doctors rx count

--- features/doctor/libs/doctor.ts
+++ features/doctor/libs/doctor.ts
@@ -5,23 +5,38 @@ import { Prisma } from "@/lib/generated/prisma";
 import { apiResponse } from "@/lib/response";
 import { baseQuerySchema } from "@/schema/base-query";
 import z from "zod";
-
+import { startOfDay, endOfDay } from "date-fns";
 
 const doctorQuerySchema = baseQuerySchema.extend({
   sapAreaCode: z.string().optional(),
+  rxDate: z.date().optional(),
 });
 
 type DoctorQueryType = z.infer<typeof doctorQuerySchema>;
 
 export type DoctorMulti = Prisma.doctorGetPayload<{
   include: {
-    _count: true;
+    _count: {
+      select: {
+        doctor_rx: true;
+      };
+    };
   };
 }>;
 
 export const getDoctors = async (query: DoctorQueryType) => {
   try {
+    const prevDate = new Date();
+    prevDate.setDate(prevDate.getDate() - 1);
+    let startDate: Date = startOfDay(prevDate);
+    let endDate: Date = endOfDay(prevDate);
+
     const validatedParams = doctorQuerySchema.parse(query);
+
+    if (validatedParams.rxDate) {
+      startDate = startOfDay(validatedParams.rxDate);
+      endDate = endOfDay(validatedParams.rxDate);
+    }
 
     const filter: Prisma.doctorWhereInput = {
       ...(validatedParams.search && {
@@ -48,14 +63,24 @@ export const getDoctors = async (query: DoctorQueryType) => {
           full_name: "asc",
         },
         include: {
-          _count: true,
+          _count: {
+            select: {
+              doctor_rx: {
+                where: {
+                  rx_date: {
+                    gte: startDate,
+                    lt: endDate,
+                  },
+                },
+              },
+            },
+          },
         },
       }),
       db.doctor.count({
         where: filter,
       }),
     ]);
-
 
     return apiResponse.multi<DoctorMulti>({
       data,
