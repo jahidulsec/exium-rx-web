d8ef5e278117038ab2ed725fb008a7847e74aace
Jahidul Islam <jahidul.app@gmail.com>
2026-05-07 18:20:12 +0600

    feat: add logout function for admin

--- components/shared/navbar/nav-user.tsx
+++ components/shared/navbar/nav-user.tsx
@@ -1,19 +1,59 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { userLogout } from "@/features/auth/actions/login";
 import { User } from "lucide-react";
 import React from "react";
+import { toast } from "sonner";
 
 export default function NavUser() {
-    return (
-        <div>
-            <Button
-                variant={"outline"}
-                className="border-primary"
-                size={"icon-lg"}
-            >
-                <User /> <span className="sr-only">Profile</span>
-            </Button>
-        </div>
-    );
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant={"outline"}
+            className="border-primary"
+            size={"icon-lg"}
+          >
+            <User /> <span className="sr-only">Profile</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuGroup>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Reset Password</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            variant="destructive"
+            onClick={() =>
+              toast.promise(userLogout, {
+                loading: "Logging out...",
+                success: data => {
+                  if (!data.success) throw data;
+                  return data.message;
+                },
+                error: data => data.message,
+              })
+            }
+          >
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
