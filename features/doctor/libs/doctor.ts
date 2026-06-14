4c8bcc59509a6c8a7e13ea2df422c72aba6f7eac
Jahidul Islam <jahidul.app@gmail.com>
2026-06-14 11:33:37 +0600

    chore: update doctor combo fetch data filter

--- features/doctor/libs/doctor.ts
+++ features/doctor/libs/doctor.ts
@@ -45,8 +45,6 @@ export const getDoctors = async (query: DoctorQueryType) => {
       endDate = endOfDay(validatedParams.rxDate);
     }
 
-    console.log(validatedParams);
-
     const isNumber = !isNaN(Number(validatedParams.search));
 
     const filter: Prisma.doctorWhereInput = {
